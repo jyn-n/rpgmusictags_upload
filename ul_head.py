f4d4113fe6dd1df1845eba876d17e0dddcd24206
jynn <uaggz@student.kit.edu>
2017-05-15 17:41:00 +0200

    splitting uploads into one upload per file

---
+++ ul_head.py
@@ -0,0 +1,48 @@
+#!/bin/python
+
+import os
+
+taglist_directory = "/usr/share/tag/"
+
+print ('IncludeLog=True\
+&User=knups\
+&Test=True\
+&TagsData={\
+Languages:[{ISO6391Code:de,Id:0}]\
+,Categories:[')
+
+
+categories = list(os.walk(taglist_directory))[0][2]
+
+def category_file (cat):
+	return taglist_directory+cat
+
+category_id = dict()
+
+for i, cat in enumerate(categories):
+	category_id[cat] = i
+
+def category_string (cat):
+	return '{Id:'+str(category_id[cat])+',Names:[{LanguageId:0,Name:'+cat+'}]}'
+
+print (','.join(map(category_string, categories)))
+
+print('],Tags:[')
+
+tags = list()
+
+for cat in categories:
+	tags += [(cat, tag.replace('\n','')) for tag in open(category_file(cat), 'r')]
+
+tag_id = dict()
+
+for i, (_, tag) in enumerate (tags):
+	tag_id[tag] = i
+
+def tag_string (cat, tag):
+	return '{Id:' + str(tag_id[tag]) + ',CategoryId:' + str(category_id[cat]) + ',Names:[{LanguageId:0,Name:' + tag + '}]}'
+
+print (','.join((tag_string(cat, tag) for cat, tag in tags)))
+
+print (']')
+
--- up.sh
+++ up.sh
@@ -2,9 +2,16 @@
 
 ul_file=$(mktemp)
 
-python up.py $1 > $ul_file
+#python up.py $1 > $ul_file
 
-[[ 0 -eq $? ]] && curl "rpgmusictags.org/Upload?format=jsv" -d @$ul_file
+python ul_mid.py $1 | while read l; do
+	python ul_head.py > $ul_file
+	echo $l >> $ul_file
+	python ul_tail.py >> $ul_file
+	[[ 0 -eq $? ]] && curl "rpgmusictags.org/Upload?format=jsv" -d @$ul_file
+	#cat $ul_file
+	#break
+done
 
 rm $ul_file
 
